8f239fd860af14851ae8b56962b9d298851c9dec
funnymmynnuf <funnymmynnuf>
2021-09-10 16:52:24 +0000

    add attributes address to deploy script

--- deploy/Guild.ts
+++ deploy/Guild.ts
@@ -13,6 +13,6 @@ module.exports = async ({
     chainId: 250,
     // gas: 4000000,
     // Set your own guild master
-    args: ["0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb", "0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2", 460172],
+    args: ["0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb", "0x2069B76Afe6b734Fb65D1d099E7ec64ee9CC76B2", "0xB5F5AF1087A8DA62A23b08C00C6ec9af21F397a1",  460172],
   });
 };
